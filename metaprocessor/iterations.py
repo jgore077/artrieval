9f2769e1ac43a002f637ffa7516e831d5425deee
“Evan <evan.koenig@maine.edu>
2024-11-17 20:34:24 -0500

    fix for directory issue

--- metaprocessor/iterations.py
+++ metaprocessor/iterations.py
@@ -110,7 +110,11 @@ def findDuplicateQuerys(metadata:dict)->tuple[dict[str,list],dict[str,list]]:
     # We filter out single occurences with these comprehensions, only returning querys with 2 or more duplicates
     return {k: v for k, v in visual.items() if len(v) > 1},{k: v for k, v in as_is.items() if len(v) > 1}
 
-def setSplit(metadata:dict):
+import os
+import random
+import json
+
+def setSplit(metadata: dict):
     processed_data = {
         str(key): " ".join(entry["visual"].values()) 
         for key, entry in metadata.items() if "visual" in entry
@@ -120,12 +124,11 @@ def setSplit(metadata:dict):
     keys = list(processed_data.keys())
     random.shuffle(keys)
     
-    # Calculate split sizes
+
     total = len(keys)
     train_size = int(total * .9)
     val_size = int(total * .05)
     
-    # Split the data
     train_keys = keys[:train_size]
     val_keys = keys[train_size:train_size + val_size]
     test_keys = keys[train_size + val_size:]
@@ -134,13 +137,16 @@ def setSplit(metadata:dict):
     val_data = {metadata[key]['file_path']: processed_data[key] for key in val_keys}
     test_data = {metadata[key]['file_path']: processed_data[key] for key in test_keys}
     
-    # Save the splits into separate JSON files
-    with open("train.json", "w") as f:
+    splits_folder = 'data/splits'
+    os.makedirs(splits_folder, exist_ok=True)
+
+    with open(os.path.join(splits_folder, "train.json"), "w") as f:
         json.dump(train_data, f, indent=4, ensure_ascii=False)
-    with open("val.json", "w") as f:
+    with open(os.path.join(splits_folder, "val.json"), "w") as f:
         json.dump(val_data, f, indent=4, ensure_ascii=False)
-    with open("test.json", "w") as f:
+    with open(os.path.join(splits_folder, "test.json"), "w") as f:
         json.dump(test_data, f, indent=4, ensure_ascii=False)
+
 
 ITERATION_DICT={
     "predictionsUncertaintyCheck":predictionsUncertaintyCheck
