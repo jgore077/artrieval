cf6a79128b3c6fa700809d89b79668073bc0949d
aidan073 <aidanbell073@gmail.com>
2024-11-18 21:55:42 -0500

    Minor bug fix in a script, obtained "counts.json" using the pipeline in "meta_processing.py". *This is not finalized counts since our dataset is not finalized*

--- meta_processing.py
+++ meta_processing.py
@@ -1,5 +1,8 @@
 from metaprocessor import *
 
-p=processor.Processor("metadata.json", delete=True)
+p=processor.Processor("data/splits/test_original.json", delete=True)
 p.mutate(visualContextualBins)
-p.mutate(removeEmptyObjects,"test.json")
+p.mutate(removeEmptyObjects)
+visual_dupes, as_is_dupes = p.iterate(findDuplicateQuerys)
+p.mutate(removeDuplicates, "test1.json", visual_dupes)
+p.iterate(countBinFields, "counts.json")
