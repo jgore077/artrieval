d62d20aef7316c97158653eca18593db7d5c8a9e
James Gore <james.gore@maine.edu>
2024-12-07 12:36:59 -0500

    calculate p@1 with matrix

--- eval.py
+++ eval.py
@@ -1,15 +1,21 @@
 from long_clip.Evaluator import Evaluator
 from metaprocessor import assemble_visual_queries
 import torch
+import json
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 evaluator=Evaluator(
-    "./long_clip/checkpoints/longclip-B.pt",
+    "./long_clip/checkpoints/longclip-L.pt",
     "data/splits/test_original.json",
     "image_embeddings/test.pt",
     ["precision@1","mrr"],
     device
 )
 
-scores=evaluator.search(assemble_visual_queries("data/splits/test_original.json"))
-print("p@1",evaluator.precision_at_1(scores))
+queries=assemble_visual_queries("data/splits/test_original.json")
+scores=evaluator.search(queries)
+score,queries=evaluator.precision_at_1(scores,queries=queries)
+print("p@1",score)
+
+with open("results.json","w",encoding="utf-8") as results_file:
+    results_file.write(json.dumps(queries,indent=4,ensure_ascii=False))
