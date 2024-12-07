112e495842fef9a2a99d9421bf72c4e7ccf5b15d
James Gore <james.gore@maine.edu>
2024-12-07 13:51:39 -0500

    modify eval

--- eval.py
+++ eval.py
@@ -6,14 +6,16 @@ import json
 device = "cuda" if torch.cuda.is_available() else "cpu"
 evaluator=Evaluator(
     "./long_clip/checkpoints/longclip-L.pt",
-    "data/as_is_v_visual/visual.json",
+    "bins.json",
     "data/as_is_v_visual/visual.tsv",
     "image_embeddings/test.pt",
     ["precision@1","mrr"],
     device
 )
 
-queries=assemble_visual_queries("data/splits/test_original.json")
+with open("data/as_is_v_visual/visual.json",encoding="utf-8") as visual_file:
+    queries=json.load(visual_file)
+    
 scores=evaluator.search(queries)
 score,queries=evaluator.precision_at_1(scores,queries=queries)
 print("p@1",score)
