5c524d1a912ced22b8f8195f8290eba735da88c3
aidan073 <aidanbell073@gmail.com>
2024-12-08 14:11:17 -0500

    added query generator util

--- metaprocessor/utils.py
+++ metaprocessor/utils.py
@@ -41,7 +41,15 @@ def write_qrel(qrels_path,qrels):
         writer=csv.writer(qrel_file, delimiter='\t',quoting=csv.QUOTE_MINIMAL)
         for row in qrels:
             writer.writerow(row)
-            
+
+def metadata_to_qrel(qrels_path, metadata):
+    """
+    Take a metadata file (.json) and get a qrel file (.tsv) *assuming only true sample is self
+    """
+    qrel_2d = []
+    for sample_id, sample in metadata.items():
+        qrel_2d.append([sample_id, 0, sample_id, 1])
+    write_qrel(qrels_path,qrel_2d)
 
 def write_querys(querys_path,querys):
     """
